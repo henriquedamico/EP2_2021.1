2baba30bf0a34e1bb87e14012714ce3e9c7d1b92
henriquedamico <81169582+henriquedamico@users.noreply.github.com>
2021-04-27 18:48:49 -0300

    Create programa_rascunho.py

---
+++ programa_rascunho.py
@@ -0,0 +1,75 @@
+from random import shuffle
+
+def cria_baralho():
+    naipes = ['♠', '♥', '♣', '♦']
+    valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    l = [valor + naipe for naipe in naipes for valor in valores]
+    shuffle(l)
+    return l
+
+def extrai_naipe(carta):
+    return carta[-1]
+
+def extrai_valor(carta):
+    return carta[:-1]
+
+def lista_movimentos_possiveis(baralho, i):
+    mov = []
+    if i < 3 and i > 0:
+        if extrai_valor(baralho[i]) == extrai_valor(baralho[i - 1]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i - 1]):
+            mov.append(1)
+    elif i >= 3:
+         if extrai_valor(baralho[i]) == extrai_valor(baralho[i - 1]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i - 1]):
+            mov.append(1)
+         if extrai_valor(baralho[i]) == extrai_valor(baralho[i - 3]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i - 3]):
+            mov.append(3)
+    return mov
+
+def empilha(baralho, o, d):
+    baralho[d] = baralho.pop(o)
+    return baralho
+
+def possui_movimentos_possiveis(baralho):
+    for i in baralho:
+        if baralho.index(i) < 3 and baralho.index(i) > 0:
+            if extrai_valor(i) == extrai_valor(baralho[baralho.index(i) - 1]) or extrai_naipe(i) == extrai_naipe(baralho[baralho.index(i) - 1]):
+                return True
+        elif baralho.index(i) >= 3:
+             if extrai_valor(i) == extrai_valor(baralho[baralho.index(i) - 1]) or extrai_naipe(i) == extrai_naipe(baralho[baralho.index(i) - 1]):
+                return True
+             if extrai_valor(i) == extrai_valor(baralho[baralho.index(i) - 3]) or extrai_naipe(i) == extrai_naipe(baralho[baralho.index(i) - 3]):
+                return True
+    return False
+
+def loop_cartas():
+    cartas = ''
+    print('O estado atual das cartas é: ')
+    for i in baralho:
+        cartas += '{}. {} \n'.format(baralho.index(i) + 1, i)
+        print(cartas)
+
+comeca = input('Aperte [Enter] para iniciar o jogo...')
+
+if comeca == '':
+    baralho = cria_baralho()
+    cartas = ''
+    for i in baralho:
+        cartas += '{}. {} \n'.format(baralho.index(i) + 1, i)
+    print(cartas)
+
+if possui_movimentos_possiveis(baralho):
+    escolha = int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
+    escolha = escolha - 1
+    movimentos_possiveis = (lista_movimentos_possiveis(baralho, escolha))
+    print(movimentos_possiveis)
+    if len(movimentos_possiveis) == 1:
+        baralho = empilha(baralho, escolha, escolha - movimentos_possiveis[0])
+        loop_cartas()
+    elif len(movimentos_possiveis) == 2:
+        escolha2 = int(input('Sobre qual carta você quer empilhar o {} ? \n 1. {}\n 2. {}\nDigite o número da sua escolha: '.format(baralho[escolha], baralho[escolha - 1], baralho[escolha - 3])))
+        if escolha2 == 1:
+            baralho = empilha(baralho, escolha, escolha - 1)
+            loop_cartas()
+        elif escolha2 == 2:
+            baralho = empilha(baralho, escolha, escolha - 3)
+            loop_cartas()
